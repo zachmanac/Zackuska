a333765713470fa2cadef4c16b07567a31537eac
zachmanac <zachstephenson94@gmail.com>
2023-05-09 13:59:31 -0700

    add image border-radius

--- customer-app/src/Components/FoodTruckList.js
+++ customer-app/src/Components/FoodTruckList.js
@@ -14,7 +14,7 @@ function FoodTruckList(props) {
         {foodTrucks.map((truck) => (
           <div className="food-truck" li key={truck.owner_id} onClick={() => handleFoodTruckClick(truck)}>
             <h3 className="food-truck-orange">{truck.truck_name}</h3>
-            <p>Phone Number: {truck.phone_number}</p>
+            <p>{truck.phone_number}</p>
             <img src={truck.picture} alt={truck.truck_name} />
             <p>Facebook: {truck.facebook}</p>
             <p>Instagram: {truck.instagram}</p>
