b6590205d2dedad94dbc90e666458dbd1e36f9c0
zachmanac <zachstephenson94@gmail.com>
2023-05-21 13:23:52 -0700

    add more seed data to db

--- server/database/seeds/02_trucks.sql
+++ server/database/seeds/02_trucks.sql
@@ -4,4 +4,5 @@ VALUES
   (3,'Poutine and More','1-618-744-4962','https://images.pexels.com/photos/730129/pexels-photo-730129.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1','facebook.com','instagram.com','Canadian', 'Toronto'),
   (5,'Fish and Chips','1-451-483-2576','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSifkFT4-NrH3e0rHC0sui9RwQz69k3eflh1Q&usqp=CAU','facebook.com','instagram.com','British', 'Toronto'),
   (6,'The Chicken and Rice Guys','(672) 468-4184','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaEwXTOTXNL3P61eKRZgVzf33XHix8A7VWOg&usqp=CAU','facebook.com','instagram.uk','Texan', 'Toronto'),
-  (7,'Los Tacos','(384) 435-3287','https://images.pexels.com/photos/1264937/pexels-photo-1264937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1','facebook.com','instagram.com','Mexican', 'Toronto');
+  (7,'Los Tacos','(384) 435-3287','https://images.pexels.com/photos/1264937/pexels-photo-1264937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1','facebook.com','instagram.com','Mexican', 'Toronto'),
+  (1, 'Burger Palace', '1-815-368-1414', 'https://images.pexels.com/photos/4609201/pexels-photo-4609201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 'facebook.com', 'instagram.com', 'American', 'Toronto');
