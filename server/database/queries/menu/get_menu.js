09f44d8c20b64e98d0f1b695863591b276739b95
zachmanac <zachstephenson94@gmail.com>
2023-05-22 21:20:24 -0700

    add seed data to menu, schedules, update getmenu query to sort alphabetically

--- server/database/queries/menu/get_menu.js
+++ server/database/queries/menu/get_menu.js
@@ -3,7 +3,7 @@ const db = require('../../connection');
 const listMenuItems = async function(truck_id){
   const queryParam=[truck_id];
   try {
-    const result = await db.query(`SELECT * FROM menu_items WHERE truck_id=$1`, queryParam);
+    const result = await db.query(`SELECT * FROM menu_items WHERE truck_id=$1 ORDER BY item_name ASC`, queryParam);
     console.log(result.rows); //all menu_items from a given truck
     return result.rows;
   } catch (err) {
